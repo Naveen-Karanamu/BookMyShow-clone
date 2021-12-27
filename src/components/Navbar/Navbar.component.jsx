a748fbeae7c7563156579709754f08ff93118886
Your Name <you@example.com>
2021-12-27 18:07:09 +0530

    Navbar is built for the xl device interface

--- src/components/Navbar/Navbar.component.jsx
+++ src/components/Navbar/Navbar.component.jsx
@@ -36,7 +36,7 @@ const NavMd = () => {
 const NavLg = () => {
     return (
         <>
-            <div className="h-20 flex items-center container mx-auto px-4 ">
+            <div className="h-16 flex items-center container mx-auto px-10 ">
                 <div className=" flex items-center w-full">
                     <img className="bg-transparent h-12 pr-5" src="https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png" alt="logo" />
                     <div className="text-gray-500 bg-white h-9 rounded-l border-0 flex items-center px-3"><IoSearchOutline className="w-4 h-4 border-0 bg-transparent" /></div>
@@ -44,7 +44,26 @@ const NavLg = () => {
                 </div>
                 <div className="flex items-center w-full gap-6 justify-end">
                     <p className="text-white flex items-center gap-1">Rajamahendravaram (Rajahmundry) <FaChevronDown className="w-3 h-3"/></p>
-                    <button className="bg-red-500 text-white px-4 py-2  rounded-md text-xs font-semibold">Sign In</button>
+                    <button className="bg-red-500 text-white px-4 py-1.5  rounded-md text-xs font-semibold">Sign In</button>
+                    <VscMenu className="text-white w-5 h-5"/>
+                </div>
+            </div>
+        </>
+    )
+}
+
+const NavXl = () => {
+    return (
+        <>
+            <div className="h-16 flex items-center container mx-auto px-28 ">
+                <div className=" flex items-center w-full">
+                    <img className="bg-transparent h-12 pr-5" src="https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png" alt="logo" />
+                    <div className="text-gray-500 bg-white h-9 rounded-l border-0 flex items-center px-3"><IoSearchOutline className="w-4 h-4 border-0 bg-transparent" /></div>
+                    <input type="search" placeholder="Search for Movies, Events, Plays, Sports and Activities" className="w-4/5 h-9 rounded-r border-0  border-none focus:outline-none" />
+                </div>
+                <div className="flex items-center w-full gap-6 justify-end">
+                    <p className="text-white flex items-center gap-1">Rajamahendravaram (Rajahmundry) <FaChevronDown className="w-3 h-3"/></p>
+                    <button className="bg-red-500 text-white px-4 py-1.5  rounded-md text-xs font-semibold">Sign In</button>
                     <VscMenu className="text-white w-5 h-5"/>
                 </div>
             </div>
@@ -60,7 +79,9 @@ const Navbar = () => {
 
                 <div className="hidden md:flex lg:hidden"><NavMd /></div>
 
-                <div className="hidden lg:flex"><NavLg /></div>
+                <div className="hidden lg:flex xl:hidden"><NavLg /></div>
+
+                <div className="hidden  xl:flex"><NavXl /></div>
             </nav>
         </>
     )
