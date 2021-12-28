9d8a5e14b3bc2deb4f0f90f8b07f32c1926a7b3d
Your Name <you@example.com>
2021-12-28 17:23:38 +0530

    Premier shows section is built for the pc interface

--- src/Pages/Homepage/Homepage.component.jsx
+++ src/Pages/Homepage/Homepage.component.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import EntertainmentCardSlider from "../../components/Entertainment/Entertainment.component";
+import Premiers from "../../components/Premier/Premiers.component";
 
 const Homepage = () => {
     return (
         <>
-            <div className="container mx-auto px-36">
-                <h1 className="text-2xl font-bold text-gray-800">The Best of Entertainment</h1>
+            <div className="container mx-auto px-36 my-14">
+                <h1 className="text-2xl font-bold text-gray-800 pb-2">The Best of Entertainment</h1>
                 <EntertainmentCardSlider />
+                <Premiers />
             </div>
         </>
     )
