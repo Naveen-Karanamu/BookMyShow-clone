1d2d27d88caf2270ddb2dcf3096cb3f88ff059f3
Your Name <you@example.com>
2022-01-03 22:03:11 +0530

    Enabled the navigation

--- src/components/Poster/Poster.component.jsx
+++ src/components/Poster/Poster.component.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Poster=(props)=>{
     return(
-        <>
+        <Link to={`/stream/{props.id}`}>
             <div className="flex flex-col items-start  lg:gap-2 px-3 ">
                 <div h-80>
                     <img src={`http://image.tmdb.org/t/p/original${props.poster_path}`} alt={props.original_title} className="w-full h-full rounded-lg"/>
@@ -11,7 +11,7 @@ const Poster=(props)=>{
                 <p className={` text-xs lg:text-sm  ${props.isDark ?"text-white" :"text-gray-700"}`}>{props.subtitle}</p>
             </div>
 
-        </> 
+        </Link> 
     );
 };
 
