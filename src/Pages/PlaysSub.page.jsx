8380ec8b940ea013d5729d16c9f2dcaed7a42acb
Your Name <you@example.com>
2022-01-31 19:58:06 +0530

    Built the Pc UI for Plays Substitute Page

--- src/Pages/PlaysSub.page.jsx
+++ src/Pages/PlaysSub.page.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { AiOutlineCalendar, AiFillLike } from "react-icons/ai"
 import { IoLocationOutline } from "react-icons/io5"
-import { FaLanguage } from "react-icons/fa"
+import { FaLanguage, FaFacebookF, FaTwitter } from "react-icons/fa"
 import { BiTime } from "react-icons/bi"
 
-const PlaysSubPage = () => {
+const PlaysSubPageMd = () => {
     return (<>
-        <div className="lg:container lg:mx-auto lg:px-48">
+        <div >
             <div className="w-full h-52 lg:h-full md:h-96">
                 <img src="https://in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-free-fire-solo-and-squad-bermuda-0-2020-12-8-t-13-48-37.jpg" className="w-full h-full" />
             </div>
@@ -85,6 +85,97 @@ const PlaysSubPage = () => {
                 </div>
             </div>
         </div>
+    </>)
+}
+
+const PlaysSubPageLg = () => {
+    return (<>
+        <div className="">
+            <div className="w-full h-52 lg:h-full md:h-96 lg:container lg:mx-auto lg:px-48 relative">
+                <img src="https://in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-free-fire-solo-and-squad-bermuda-0-2020-12-8-t-13-48-37.jpg" className="w-full h-full" />
+
+                <div className="bg-white p-4">
+                    <div className="flex  items-center justify-between">
+                        <div>
+                            <h1 className="text-2xl font-bold text-navBarBMS-800 ">Free Fire Solo And Squad (Bermuda)</h1>
+                            <div className="flex flex-col gap-1">
+                                <h1>E Sports | English | 16yrs +</h1>
+                            </div>
+                        </div>
+                        <div>
+                            <button className="text-white bg-commonPink-200 text-lg py-4 px-12 rounded">Register</button>
+                        </div>
+                    </div>
+                    <div className="p-4">
+                        <hr ></hr>
+                    </div>
+                    <div className="flex gap-8 text-navBarBMS-900">
+                        <div className="flex gap-3 item-center">
+                            <AiOutlineCalendar className="w-5 h-5 text-navBarBMS-400 " />
+                            <h3 className="text-navBarBMS-800">Sat 05 Feb 2022 - Sum 27 Feb 2022</h3>
+                        </div>
+                        <div className="flex gap-3 item-center">
+                            <IoLocationOutline className="w-6 h-6 text-amber-400 " />
+                            <h3 className="text-navBarBMS-800">Online (Play From Home)    |    <span className="font-medium">₹ 100</span> <span className="text-xs">Onwards</span></h3>
+                        </div>
+                    </div>
+                </div>
+                <div className="bg-white p-5 absolute mt-6 w-64">
+                        <h2 className="text-xl font-semibold text-navBarBMS-900 ">Share this event</h2>
+                        <div className="flex gap-4 mt-3">
+                            <FaFacebookF className="w-5 h-5  text-navBarBMS-600"/>
+                            <FaTwitter className="w-5 h-5  text-navBarBMS-600"/>
+                        </div>
+                    </div>
+            </div>
+            <div className="">
+                <div className="container mx-auto  py-6 " style={{ width: "calc(37vw" }}>
+                    
+                    <div className="bg-white p-4 mb-6">
+                        <h1 className="text-lg font-medium pb-3">Click on Interested to stay updated about this event.</h1>
+                        <div className="flex items-center justify-between">
+                            <div className="flex flex-col">
+                                <div className="flex items-center gap-2">
+                                    <AiFillLike className="text-lime-600 w-6 h-6" />
+                                    <span>486</span>
+                                </div >
+                                <p className="text-navBarBMS-700 pt-1 w-4/5 text-xs">People have shown interest recently</p>
+                            </div>
+                            <div >
+                                <button className="border-2 px-10 h-10 text-md text-commonPink-200 border-commonPink-200 rounded">Interested?</button>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="bg-white p-5">
+                        <div className="bg-commonPink-300 flex flex-col gap-3 p-5 rounded-lg">
+                            <h3 className="text-lg font-medium pb-1">NOTE</h3>
+                            <h2 className="text-xl font-bold text-navBarBMS-900 ">PRIZE MONEY UPTO 8 Lakhs</h2>
+                            <p className="text-navBarBMS-900 text-sm">Provide immediate relied and support to underserved comminitied affected by COVID-19.</p>
+                            <span className="text-sm font-semibold text-commonPink-200">To Donate Now, Click Here.</span>
+                        </div>
+                        <div className="pt-7">
+                            <h2 className="text-xl font-medium text-navBarBMS-700 ">About</h2>
+                            <h4 className="text-navBarBMS-800 text-md py-2">Succession of previous tournament offering a fair opportunity to earn Rs.</h4>
+                            <h4 className="text-navBarBMS-800 text-md py-2">Prized offered:-</h4>
+                            <span className="text-sm font-normal text-commonPink-200">Read More</span>
+                        </div>
+
+                    </div>
+
+                    {/* <PosterSlider images={} title="Explore Fun Activities" isDark={false} /> */}
+
+                </div>
+            </div>
+        </div>
+    </>)
+}
+
+const PlaysSubPage = () => {
+    return (<>
+        <div className="lg:hidden"><PlaysSubPageMd /></div>
+        <div className="lg:block">< PlaysSubPageLg /></div>
     </>)
 }
 
