c3b99e14a955c0456fd16e57243b5d52d8377d6d
Your Name <you@example.com>
2022-02-01 00:23:49 +0530

    Modified the extended  Navbar

--- src/components/Navbar/Extended.Navbar.jsx
+++ src/components/Navbar/Extended.Navbar.jsx
@@ -1,22 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ExtendedNav = () => {
     return (<>
         <div className="hidden lg:flex justify-around bg-commonPink-400 h-10 items-center">
             <div className="flex gap-4 text-gray-300 text-sm font-medium">
-                <p>Movies</p>
-                <p>Stream</p>
-                <p>Events</p>
-                <p>Plays</p>
-                <p>Sports</p>
-                <p>Activities</p>
-                <p>Buzz</p>
+                <p className="cursor-pointer hover:text-white">Movies</p>
+                <p className="cursor-pointer hover:text-white">Stream</p>
+                <p className="cursor-pointer hover:text-white">Events</p>
+                <p className="cursor-pointer hover:text-white" >Plays</p>
+                <p className="cursor-pointer hover:text-white">Sports</p>
+                <p className="cursor-pointer hover:text-white">Activities</p>
+                <p className="cursor-pointer hover:text-white">Buzz</p>
             </div>
             <div className="flex gap-4 text-gray-300 text-sm font-medium">
-            <p>ListYourShow</p>
-            <p>Corporates</p>
-            <p>Offers</p>
-            <p>Gift Cards</p>
+                <p className="cursor-pointer hover:text-white">ListYourShow</p>
+                <p className="cursor-pointer hover:text-white">Corporates</p>
+                <p className="cursor-pointer hover:text-white">Offers</p>
+                <p className="cursor-pointer hover:text-white">Gift Cards</p>
             </div>
         </div>
     </>)
