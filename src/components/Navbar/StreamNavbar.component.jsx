2c799e913de4837b1c96bb0f14b3b185231f3943
Naveen Kumar <karanamunaveen2002@gmail.com>
2022-04-26 14:20:08 +0530

    Updated: New changes

--- src/components/Navbar/StreamNavbar.component.jsx
+++ src/components/Navbar/StreamNavbar.component.jsx
@@ -8,16 +8,18 @@ import { FaChevronDown } from "react-icons/fa"
 import Logo from "../../images/logo.image";
 import { MovieContext } from "../../context/movie.context";
 const NavSm = () => {
-    const {movie}=useContext(MovieContext)
+    const { movie } = useContext(MovieContext)
     return (
         <>
-            <div className="flex items-center justify-between h-14 sticky top-0 z-10 w-full">
-                <div className="px-4 py-2 flex items-center gap-6">
-                    <span className="text-white "><BsChevronLeft className="w-4 h-4" /></span>
-                    <h1 className="text-md font-semibold text-white">{movie.original_title}</h1>
-                </div>
-                <div className="flex items-center gap-4 px-4">
-                    <div className="text-white w-6 h-6"><IoShareSocialOutline className="w-full h-full" /></div>
+            <div className="relative">
+                <div className="flex items-center justify-between h-14 sticky top-0  z-10 w-full">
+                    <div className="px-4 py-2 flex items-center gap-6">
+                        <span className="text-white "><BsChevronLeft className="w-4 h-4" /></span>
+                        <h1 className="text-md font-semibold text-white">{movie.original_title}</h1>
+                    </div>
+                    <div className="flex items-center gap-4 px-4">
+                        <div className="text-white w-6 h-6"><IoShareSocialOutline className="w-full h-full" /></div>
+                    </div>
                 </div>
             </div>
         </>
@@ -62,7 +64,7 @@ const NavXl = () => {
                     {/* <img className="bg-transparent h-12 pr-5" src="https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png" alt="logo" /> */}
                     <Logo />
                     <div className="text-gray-500 bg-white h-9 rounded-l border-0 flex items-center px-3"><IoSearchOutline className="w-4 h-4 border-0 bg-transparent" /></div>
-                    <input type="search" placeholder="Search for Streams, Events, Plays, Sports and Activities" className=" h-9 rounded-r border-0  border-none focus:outline-none"style={{width:"650px"}} />
+                    <input type="search" placeholder="Search for Streams, Events, Plays, Sports and Activities" className=" h-9 rounded-r border-0  border-none focus:outline-none" style={{ width: "650px" }} />
                 </div>
                 <div className="flex items-center w-full gap-6 justify-end">
                     <p className="text-white flex items-center gap-1"> Vishakapatnam <FaChevronDown className="w-3 h-3" /></p>
