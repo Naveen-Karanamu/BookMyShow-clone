bd987a31b4d3e4810e5acf7a1780dd7a8b52ab8e
Naveen Kumar <karanamunaveen2002@gmail.com>
2022-04-28 22:36:42 +0530

    Added: Mobile Navigation bar

--- src/components/Footer/Mobile.Footer.jsx
+++ src/components/Footer/Mobile.Footer.jsx
@@ -1,5 +1,6 @@
 import react from "react";
 import { Link } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 
 // Icons
 import { BiCameraMovie, BiMoviePlay, BiCalendarEvent, BiFootball } from 'react-icons/bi'
@@ -9,6 +10,49 @@ import { FaRunning } from 'react-icons/fa'
 
 
 const MobileFooter = () => {
+    const location = useLocation();
+    const currentPath = location.pathname;
+    // console.log(currentPath);
+
+    const tabs = [
+        {
+            title: "movies",
+            route: "movies",
+            isActive: currentPath.includes("movies")
+        },
+        {
+            title: "Order Online",
+            route: "order-online",
+            isActive: currentPath.includes("order-online")
+        },
+        {
+            title: "events",
+            route: "events",
+            isActive: currentPath.includes("events")
+        },
+        {
+            title: "plays",
+            route: "plays",
+            isActive: currentPath.includes("plays")
+        },
+        {
+            title: "sports",
+            route: "sports",
+            isActive: currentPath.includes("sports")
+        },
+        {
+            title: "activities",
+            route: "activities",
+            isActive: currentPath.includes("activities")
+        },
+        {
+            title: "buzz",
+            route: "buzz",
+            isActive: currentPath.includes("/buzz")
+        }
+
+    ]
+
     return (
         <>
             <div className="md:hidden bg-gray-100 w-full h-16 fixed -bottom-3 z-10 text-gray-600 pt-2 border-t border-gray-300">
@@ -32,7 +76,7 @@ const MobileFooter = () => {
                         <FaRunning className="w-7 h-7" />
                     </Link>
                     <Link to="/buzz">
-                        <HiOutlineSpeakerphone className="w-7 h-7" />
+                        <HiOutlineSpeakerphone className={tabs.isActive?"w-7 h-7 bg-red-600":"w-7 h-7"} />
                     </Link>
                 </div>
             </div>
