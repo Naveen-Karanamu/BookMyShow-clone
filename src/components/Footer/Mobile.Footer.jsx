ccfa7064554908df508f35effce1caff587c2ee9
Naveen Kumar <karanamunaveen2002@gmail.com>
2022-04-28 19:18:42 +0530

    Added: Extended nav to the medium devices

---
+++ src/components/Footer/Mobile.Footer.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+
+const MobileFooter=()=>{
+    return(
+        <>
+        <div className="md:hidden">
+            Footer
+        </div>
+        </>
+    )
+}
+
+export default MobileFooter;
--- src/components/Navbar/Extended.Navbar.jsx
+++ src/components/Navbar/Extended.Navbar.jsx
@@ -39,6 +39,30 @@ const ExtendedNav = () => {
                 <p className="cursor-pointer hover:text-white">Gift Cards</p>
             </div>
         </div>
+
+        <div className="hidden md:flex lg:hidden bg-commonPink-400 h-10 items-center md:h-8 md:fixed md:top-14 md:w-full md:z-10">
+            <div className="flex gap-4 text-gray-300 text-sm font-medium px-8">
+                <Link to="/movies">
+                    <p className="cursor-pointer hover:text-white focus:text-black">Movies</p>
+                </Link>
+                <Link to="/">
+                    <p className="cursor-pointer hover:text-white">Stream</p></Link>
+                <Link to="/events">
+                    <p className="cursor-pointer hover:text-white">Events</p>
+                </Link>
+                <Link to="/plays">
+                    <p className="cursor-pointer hover:text-white">Plays</p>
+                </Link>
+                <Link to="/sports">
+                    <p className="cursor-pointer hover:text-white">Sports</p>
+                </Link>
+                <Link to="/activities">
+                    <p className="cursor-pointer hover:text-white">Activities</p></Link>
+                <Link to="/buzz">
+                    <p className="cursor-pointer hover:text-white">Buzz</p>
+                </Link>
+            </div>
+        </div>
     </>)
 }
 
